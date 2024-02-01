a760737f8a6ad145705bc9898de3effbd3df6249
HenryBondah <bondahdean@gmail.com>
2024-01-31 17:03:26 -0700

    new

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -5,11 +5,11 @@ VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 --Q2
 UPDATE account 
 SET account_type = 'Admin' 
-WHERE account_first_name = 'Tony' AND account_lastname = 'Stark';
+WHERE account_id = 1;
 
 --Q3
 DELETE FROM account 
-WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+WHERE account_id = 1;
 
 --Q4
 UPDATE inventory 
@@ -19,11 +19,10 @@ WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 --Q5
 SELECT inv_make, inv_model, c.classification_name 
 FROM inventory i 
-INNER JOIN classification c ON i.classification_id = c.classification_id 
+INNER JOIN classification c ON i.classification_id = c.classification_id
 WHERE c.classification_name = 'Sport';
 
 --6
 UPDATE inventory 
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'), 
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
-
